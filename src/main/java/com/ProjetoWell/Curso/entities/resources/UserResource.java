12d016488e06315b7a95e3e7787f3d749d283ea7
Fiwe64 <wellington.honneyfilho@gmail.com>
2025-07-25 15:09:31 -0400

    User insert

--- src/main/java/com/ProjetoWell/Curso/entities/resources/UserResource.java
+++ src/main/java/com/ProjetoWell/Curso/entities/resources/UserResource.java
@@ -5,7 +5,9 @@ import com.ProjetoWell.Curso.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,6 +31,13 @@ public class UserResource {
 
 
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){//desserializando o json para virar um objeto
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 
